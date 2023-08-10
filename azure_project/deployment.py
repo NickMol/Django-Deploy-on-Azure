95b3433a94d4bdf32852ed6acdb8619845480ad3
NickMol <nickmol96@hotmail.com>
2023-08-10 19:14:11 +0200

    addedd secret to production file

--- azure_project/deployment.py
+++ azure_project/deployment.py
@@ -2,6 +2,8 @@ import os
 from settings.py import *
 from .settings import BASE_DIR
 
+
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False 
