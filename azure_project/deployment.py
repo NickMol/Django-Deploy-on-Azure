149b64be751738f1bd8c4fc3f6314420da6ec629
NickMol <nickmol96@hotmail.com>
2023-08-12 17:22:10 +0200

    first commit

--- azure_project/deployment.py
+++ azure_project/deployment.py
@@ -6,7 +6,7 @@ from .settings import BASE_DIR
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
-DEBUG = False
+DEBUG = True
 
 # WhiteNoise configuration
 MIDDLEWARE = [
