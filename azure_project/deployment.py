2768c34206b41ed9b1354640f75b966a9ab884b6
NickMol <nickmol96@hotmail.com>
2023-08-12 16:59:56 +0200

    changes

--- azure_project/deployment.py
+++ azure_project/deployment.py
@@ -4,8 +4,8 @@ from .settings import BASE_DIR
 
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = os.environ['WEBSITE_HOSTNAME']
-CSRF_TRUSTED_ORIGINS = 'https://' + os.environ['WEBSITE_HOSTNAME']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
 # WhiteNoise configuration
