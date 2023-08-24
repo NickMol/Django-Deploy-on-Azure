8981654e47af1ed4c258507219a0b6ef5537f441
NickMol <nickmol96@hotmail.com>
2023-08-24 18:59:03 +0200

    adding secret key

--- azure_project/deployment.py
+++ azure_project/deployment.py
@@ -18,7 +18,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+] 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
